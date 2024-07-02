177362cab1a09e57dbbab218f0f5f2a943634432
gokulsk16 <gokulakrishna161@gmail.com>
2024-07-02 16:54:59 +0530

    Portfolio-site

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   }
   return (
     <div className="Navbar">
-      <h1><span>Portfolio</span></h1>
+      <h1 className="title-t"><span>Portfolio</span></h1>
       <img src={menu_open} alt=""  className="nav-open-mob" onClick={openMenu}/>
       <ul ref={menuRef} className="Navbar-menu">
         <img src={menu_close} alt="" className="nav-close-mob" onClick={closeMenu}/>
@@ -25,7 +25,7 @@ const Navbar = () => {
         <AnchorLink className="anchor-link" offset={50} href="#contact"><li>Contact</li></AnchorLink>
       </ul>
       <div className="nav-contact">
-      <AnchorLink className="anchor-link" offset={50} href="#contact">Connect with Me</AnchorLink></div>
+      {/* <AnchorLink className="anchor-link" offset={50} href="#contact">Connect with Me</AnchorLink></div> */}
     </div>
   );
 };
